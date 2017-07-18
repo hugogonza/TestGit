e857c85eae16ffa0bb67efad19501c10b9046548
AOrtizM-IW <aortiz@nterware.com.mx>
2017-07-18 13:25:42 -0500

    brnach

--- GITSAMPLE/FirstViewController.swift
+++ GITSAMPLE/FirstViewController.swift
@@ -17,13 +17,15 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("NEW BRANCH ARTURO")
-
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         //example
+        //COMMENT2
+        print("error")
     }
 
 
