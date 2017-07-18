fa433405bc0cdfcb741bcfb5f17b1da78ce935a9
Hugo <hugogonza@users.noreply.github.com>
2017-07-18 13:19:40 -0500

    "test commit"

--- GITSAMPLE/FirstViewController.swift
+++ GITSAMPLE/FirstViewController.swift
@@ -20,6 +20,7 @@ class FirstViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //example
     }
 
 
