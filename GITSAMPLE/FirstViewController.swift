7502f19427afe93eae3b66ffe4ba5932d6b84afc
AOrtizM-IW <aortiz@nterware.com.mx>
2017-07-18 11:52:59 -0500

    first commit

--- GITSAMPLE/FirstViewController.swift
+++ GITSAMPLE/FirstViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import SwiftEventBus
+import Alamofire
 
 class FirstViewController: UIViewController {
 
---
+++ Pods/Target Support Files/Pods-GITSAMPLE/Pods-GITSAMPLE-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GITSAMPLE : NSObject
+@end
+@implementation PodsDummy_Pods_GITSAMPLE
+@end
