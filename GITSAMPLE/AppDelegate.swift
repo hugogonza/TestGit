56114a1cdcc6aac751123a4c9c4f53cb41271454
cesarTix <cesarsumano@icloud.com>
2017-07-18 12:33:44 -0500

    Prueba funcion appDelegate artur cesar

--- GITSAMPLE/AppDelegate.swift
+++ GITSAMPLE/AppDelegate.swift
@@ -22,6 +22,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         changedPush()
+        
+        testChange()
+        
         return true
     }
 
